19400498c55370c169e7c77eb4223844d5fd665e
hskhsmm <hskhsmm@naver.com>
2026-02-05 00:49:32 +0900

    Initialize commit: 프로젝트 기본 세팅 (docker-compose, application.yml, build.gradle 등)

---
+++ src/main/java/io/reco/collector/RecoBiddingCollectorApplication.java
@@ -0,0 +1,13 @@
+package io.reco.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoBiddingCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoBiddingCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/reco/collector/RecoBiddingCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reco.collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoBiddingCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
