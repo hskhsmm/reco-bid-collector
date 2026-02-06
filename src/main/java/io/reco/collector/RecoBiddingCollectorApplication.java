db90055a530006e33eb300826b434ae6a2cf4468
hskhsmm <hskhsmm@naver.com>
2026-02-06 23:21:18 +0900

    feat: ScraperService 크롤링 통합 서비스 구현

--- src/main/java/io/reco/collector/RecoBiddingCollectorApplication.java
+++ src/main/java/io/reco/collector/RecoBiddingCollectorApplication.java
@@ -2,8 +2,12 @@ package io.reco.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  // 스케줄링 활성화 (@Scheduled)
+@EnableRetry       // 재시도 활성화 (@Retryable)
 public class RecoBiddingCollectorApplication {
 
 	public static void main(String[] args) {
